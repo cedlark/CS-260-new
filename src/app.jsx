b7cfe42a26e12caa03f60bb0dff939f7b69331ef
cedlark <cedricbretstephens@gmail.com>
2025-10-08 17:47:57 -0600

    Router

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Friends } from './friends/friends';
+import { Map } from './map/map';
+import { Post } from './post/post';
 
 export default function App() {
     return (
+        <BrowserRouter>
       <div className="d-flex flex-column min-vh-100 bg-light text-dark">
         <header>
           <nav className="navbar navbar-expand-lg navbar-dark w-100">
@@ -21,16 +27,16 @@ export default function App() {
               <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                   <li className="nav-item">
-                    <a className="nav-link text-white" href="index.html">Activity</a>
+                    <NavLink className='nav-link' to='login'>Login</NavLink>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link text-white" href="friends.html">Friends</a>
+                    <NavLink className='nav-link' to='friends'>Friends</NavLink>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link text-white" href="post.html">Make A Post</a>
+                    <NavLink className='nav-link' to='post'>Post</NavLink>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link text-white" href="map.html">Map</a>
+                    <NavLink className='nav-link' to='map'>Map</NavLink>
                   </li>
                 </ul>
                 <span className="text-white">Hello Username</span>
@@ -39,9 +45,14 @@ export default function App() {
           </nav>
         </header>
   
-        <main className="container my-5 text-center">
-          App Components go here
-        </main>
+        <Routes>
+        <Route path='/' element={<Login />} exact />
+        <Route path='/play' element={<Friends />} />
+        <Route path='/scores' element={<Post />} />
+        <Route path='/about' element={<Map />} />
+        <Route path='*' element={<NotFound />} />
+        </Routes>
+       
   
         <footer className="bg-dark text-white-50 text-center py-3 mt-auto">
           <div className="container-fluid">
@@ -52,6 +63,10 @@ export default function App() {
           </div>
         </footer>
       </div>
+      </BrowserRouter>
     );
+  }
+  function NotFound() {
+    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
   }
   
