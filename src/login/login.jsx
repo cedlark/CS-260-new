985331f0f1cbbce90d3b3609dbf2ab6d6771032d
cedlark <cedricbretstephens@gmail.com>
2025-11-05 17:07:08 -0700

    Trying to make login work

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,33 +1,53 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { AuthState } from './authState';
+import { Authenticated } from './authenticated';
+import { Unauthenticated } from './unauthenticated';
 
 export function Login() {
   const navigate = useNavigate();
 
-  function handleLogin(e) {
-    e.preventDefault(); 
-    navigate('/friends'); 
+  const [authState, setAuthState] = React.useState(AuthState.Unknown);
+  const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+
+  function onAuthChange(userName, newState) {
+    setUserName(userName);
+    setAuthState(newState);
   }
+
+  React.useEffect(() => {
+    if (userName) {
+      setAuthState(AuthState.Authenticated);
+    } else {
+      setAuthState(AuthState.Unauthenticated);
+    }
+  }, [userName]);
 
   return (
     <main className="container-fluid text-center">
       <h1>Welcome to Taco Baco</h1>
-      <form onSubmit={handleLogin} className="w-25 mx-auto">
-        <div>
-          {authState !== AuthState.Unknown && <h1>Welcome to Simon</h1>}
-          {authState === AuthState.Authenticated && (
-            <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
-          )}
-          {authState === AuthState.Unauthenticated && (
-            <Unauthenticated
-              userName={userName}
-              onLogin={(loginUserName) => {
-                onAuthChange(loginUserName, AuthState.Authenticated);
-              }}
-            />
-          )}
-        </div>
-      </form>
+
+      <div className="w-25 mx-auto">
+        {authState !== AuthState.Unknown && <h2>Welcome to Simon</h2>}
+
+        {authState === AuthState.Authenticated && (
+          <Authenticated
+            userName={userName}
+            onLogout={() => onAuthChange('', AuthState.Unauthenticated)}
+          />
+        )}
+
+        {authState === AuthState.Unauthenticated && (
+          <Unauthenticated
+            userName={userName}
+            onLogin={(loginUserName) => {
+              onAuthChange(loginUserName, AuthState.Authenticated);
+              navigate('/friends'); 
+            }}
+          />
+        )}
+      </div>
+
       <p>After you log in, your friends' posts go here.</p>
     </main>
   );
