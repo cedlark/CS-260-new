bf853e904f2ad939c5ef46b4fbd4426321e76e80
cedlark <cedricbretstephens@gmail.com>
2025-11-12 12:35:19 -0700

    Added database.js

--- service/index.js
+++ service/index.js
@@ -1,68 +1,136 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import bcrypt from "bcryptjs";
-import uuid from "uuid";
-
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const uuid = require('uuid');
 const app = express();
-const port = process.env.PORT || 4000;
+const DB = require('./database.js');
 
+const authCookieName = 'token';
+
+// The service port may be set on the command line
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+// JSON body parsing using built-in middleware
 app.use(express.json());
+
+// Use the cookie parser middleware for tracking authentication tokens
 app.use(cookieParser());
 
-let users = [];
-let posts = [];
-let friends = [];
+// Serve up the applications static content
+app.use(express.static('public'));
 
+// Router for service endpoints
 const apiRouter = express.Router();
+app.use(`/api`, apiRouter);
 
-apiRouter.post("/auth/create", async (req, res) => {
-  // TODO: Add user creation logic
-  res.sendStatus(200);
+// CreateAuth token for a new user
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await findUser('email', req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await createUser(req.body.email, req.body.password);
+
+    setAuthCookie(res, user.token);
+    res.send({ email: user.email });
+  }
 });
 
-apiRouter.post("/auth/login", async (req, res) => {
-  // TODO: Add login logic
-  res.sendStatus(200);
+// GetAuth token for the provided credentials
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await findUser('email', req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      user.token = uuid.v4();
+      await DB.updateUser(user);
+      setAuthCookie(res, user.token);
+      res.send({ email: user.email });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
 });
 
-apiRouter.delete("/auth/logout", async (req, res) => {
-  // TODO: Add logout logic
-  res.sendStatus(204);
+// DeleteAuth token if stored in cookie
+apiRouter.delete('/auth/logout', async (req, res) => {
+  const user = await findUser('token', req.cookies[authCookieName]);
+  if (user) {
+    delete user.token;
+    DB.updateUser(user);
+  }
+  res.clearCookie(authCookieName);
+  res.status(204).end();
 });
 
-apiRouter.get("/posts", (req, res) => {
-  // TODO: Return all posts later
-  res.sendStatus(200);
+// Middleware to verify that the user is authorized to call an endpoint
+const verifyAuth = async (req, res, next) => {
+  const user = await findUser('token', req.cookies[authCookieName]);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+};
+
+// GetScores
+apiRouter.get('/scores', verifyAuth, async (req, res) => {
+  const scores = await DB.getHighScores();
+  res.send(scores);
 });
 
-apiRouter.post("/posts", (req, res) => {
-  // TODO: Handle adding new posts later
-  res.sendStatus(201);
+// SubmitScore
+apiRouter.post('/score', verifyAuth, async (req, res) => {
+  const scores = updateScores(req.body);
+  res.send(scores);
 });
 
-apiRouter.get("/friends", (req, res) => {
-  // TODO: Return friends list later
-  res.sendStatus(200);
+// Default error handler
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
 });
 
-apiRouter.post("/friends", (req, res) => {
-  // TODO: Handle adding a friend later
-  res.sendStatus(201);
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
 });
 
-app.use("/api", apiRouter);
+// updateScores considers a new score for inclusion in the high scores.
+async function updatePosts(newScore) {
+  await DB.addScore(newScore);
+  return DB.getHighScores();
+}
 
-import path from "path";
-import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+async function createUser(email, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
 
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  await DB.addUser(user);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
-});
+  return user;
+}
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+async function findUser(field, value) {
+  if (!value) return null;
+
+  if (field === 'token') {
+    return DB.getUserByToken(value);
+  }
+  return DB.getUser(value);
+}
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    maxAge: 1000 * 60 * 60 * 24 * 365,
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
+
+const httpService = app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
